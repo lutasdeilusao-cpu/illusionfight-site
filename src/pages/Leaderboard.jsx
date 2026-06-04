1b01e9c1217455397763df595649c90eba66b984
Isaias Leal <Ipms@panoramaid.com.br>
2026-06-04 07:23:44 -0300

    fix: LoginGate import no Leaderboard

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
+import LoginGate from '../components/LoginGate/LoginGate'
 import './Leaderboard.css'
 
 const ranking = [
