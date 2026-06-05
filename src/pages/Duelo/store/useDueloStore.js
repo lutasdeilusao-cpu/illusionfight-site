9e94cb32ac0b4bea6e083bdc8ab99117e8e16295
Isaias Leal <Ipms@panoramaid.com.br>
2026-06-05 00:50:48 -0300

    fix: overflow visible carta hover v1.1.5

--- src/pages/Duelo/store/useDueloStore.js
+++ src/pages/Duelo/store/useDueloStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { createInitialState } from '../engine/gameState'
 
-export const DUELO_VERSION = '1.1.4'
+export const DUELO_VERSION = '1.1.5'
 console.log(`[DUELO] versão carregada: ${DUELO_VERSION}`)
 
 export const useDueloStore = create((set, get) => ({
--- src/pages/Duelo/version.js
+++ src/pages/Duelo/version.js
@@ -1,2 +1,2 @@
-export const DUELO_VERSION = '1.1.4'
+export const DUELO_VERSION = '1.1.5'
 console.log(`[DUELO] versão carregada: ${DUELO_VERSION}`)
