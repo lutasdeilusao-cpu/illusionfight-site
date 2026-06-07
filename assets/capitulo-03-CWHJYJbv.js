c755aaf04d9b7ace875274d6b7740d5425b0bdef
Isaias Leal <Ipms@panoramaid.com.br>
2026-06-07 01:18:13 -0300

    Updates

--- assets/capitulo-03-CWHJYJbv.js
+++ assets/capitulo-03-CWHJYJbv.js
@@ -18,7 +18,7 @@ Kim virou a tela do tablet para o próximo exercício.\r
 \r
 Kim ergueu os olhos, avaliou Brock por um segundo, e voltou para o tablet.\r
 \r
-— Aqui não é lugar pra isso.\r
+— Aceito, mas ja aviso nosso tempo é caro, e você tem que valer o esforço.\r
 \r
 Não era pergunta. Não era recusa. Era redirecionamento, dito com a calma de quem já decidiu como a cena vai terminar.\r
 \r
