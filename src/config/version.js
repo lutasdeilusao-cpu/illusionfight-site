fbe459de4666a0315088775f0c651cf64cb0e668
Isaias Leal <Ipms@panoramaid.com.br>
2026-06-09 00:32:17 -0300

    Hero slideshow new copywriting + v9.31

--- src/config/version.js
+++ src/config/version.js
@@ -8,7 +8,7 @@
  */
 
 // ── Site ──────────────────────────────────────────
-export const SITE_VERSION = '9.30'
+export const SITE_VERSION = '9.31'
 
 // ── Games ─────────────────────────────────────────
 export const PP_VERSION        = '1.7.1'  // Pesadelo Particular
