d1ba73d78ea0a9ace3f9e1a99b9a025846188e68
Isaias Leal <Ipms@panoramaid.com.br>
2026-09-10 16:19:47 -0300

    Updates

---
+++ assets/Puzzles-DHscsn8V.js
@@ -0,0 +1,1 @@
+import"./PuzzleDecoder-CigO_r0R.js";import"./PuzzleSimonSays-v1AD9bIy.js";import"./PuzzleAnagrama-CzJv88QS.js";
--- assets/Puzzles-WaWYtXzS.js
+++
@@ -1,1 +0,0 @@
-import"./PuzzleDecoder-DKwF57Jl.js";import"./PuzzleSimonSays-MS8W0P06.js";import"./PuzzleAnagrama-zw5wRVtH.js";
