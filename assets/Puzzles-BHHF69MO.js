da7cbad02689b97c022cd925ead04f38dc8fed6c
Isaias Leal <Ipms@panoramaid.com.br>
2026-09-21 21:50:13 -0300

    Deploy v10.280.239 (fix: agiota dentro da birosca)

---
+++ assets/Puzzles-BHHF69MO.js
@@ -0,0 +1,1 @@
+import"./PuzzleDecoder-Dg8aHMYQ.js";import"./PuzzleSimonSays-C1R1odfW.js";import"./PuzzleAnagrama-ieMoPZgP.js";
--- assets/Puzzles-C9q7Zycr.js
+++
@@ -1,1 +0,0 @@
-import"./PuzzleDecoder-SmHiEAec.js";import"./PuzzleSimonSays-BQloKivp.js";import"./PuzzleAnagrama-C_3GbeZZ.js";
